948e59b6191e18622d55834198e0bea0ca87da4d
patel231 <123609651+patel231@users.noreply.github.com>
2023-07-07 13:40:09 -0400

    add

---
+++ M06-ACT01-Implementing  Queue/src/Queue.java
@@ -0,0 +1,41 @@
+public class Queue<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void enqueue(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T item = head.data;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
